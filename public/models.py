4ed0999b46af565c568a1fc26206246619bcebde
nikit <nikitph@gmail.com>
2016-04-07 17:02:44 -0700

    icons

--- public/models.py
+++ public/models.py
@@ -65,16 +65,16 @@ class Student(db.Document):
 
 
 class Parent(db.Document):
-    relationship = db.StringField(required=True, max_length=20, help_text='')
+    relationship = db.StringField(required=True, max_length=20, help_text='supervisor_account')
     parent_name = db.StringField(required=True, max_length=20, help_text='perm_identity')
     student_id = db.StringField(required=True, max_length=50, help_text='')
     street_address = db.StringField(required=True, help_text='location_on')
-    city = db.StringField(required=True, max_length=20, help_text='')
-    state = db.StringField(required=True, max_length=20, help_text='')
-    pincode = db.StringField(required=True, max_length=20, help_text='')
-    annual_income = db.StringField(required=True, max_length=50, help_text='')
-    occupation = db.StringField(required=True, max_length=50, help_text='')
-    phone = db.StringField(required=True, max_length=20, help_text='')
+    city = db.StringField(required=True, max_length=20, help_text='location_city')
+    state = db.StringField(required=True, max_length=20, help_text='navigation')
+    pincode = db.StringField(required=True, max_length=20, help_text='local_parking')
+    annual_income = db.StringField(required=True, max_length=50, help_text='monetization_on')
+    occupation = db.StringField(required=True, max_length=50, help_text='work')
+    phone = db.StringField(required=True, max_length=20, help_text='phone')
     email = db.StringField(required=True, max_length=20, help_text='email')
 
     def save(self, *args, **kwargs):
@@ -114,13 +114,13 @@ class Award(db.Document):
 
 
 class Subject(db.Document):
-    code = db.StringField(required=True, max_length=50, help_text='')
-    subject_name = db.StringField(required=True, max_length=50, help_text='')
-    books = db.StringField(required=True, max_length=50, help_text='')
-    syllabus = db.StringField(required=True, max_length=50, help_text='')
-    total_theory_hours = db.IntField(required=True)
-    class_duration = db.IntField(required=True)
-    description = db.StringField(required=True)
+    code = db.StringField(required=True, max_length=50, help_text='code')
+    subject_name = db.StringField(required=True, max_length=50, help_text='book')
+    books = db.StringField(required=True, max_length=50, help_text='library_books')
+    syllabus = db.StringField(required=True, max_length=50, help_text='content_paste')
+    total_theory_hours = db.IntField(required=True, help_text='hourglass_empty')
+    class_duration = db.IntField(required=True, help_text='hourglass_full')
+    description = db.StringField(required=True, help_text='description')
     school = db.StringField(required=True, max_length=50, help_text='')
 
     def __str__(self):
@@ -130,12 +130,12 @@ class Subject(db.Document):
 
 
 class Teacher(db.Document):
-    teacher_name = db.StringField(required=True, max_length=50, help_text='')
+    teacher_name = db.StringField(required=True, max_length=50, help_text='perm_identity')
     gender = db.BooleanField(required=True)
-    street_address = db.StringField(required=True)
-    city = db.StringField(required=True, max_length=20, help_text='')
-    state = db.StringField(required=True, max_length=20, help_text='')
-    pincode = db.StringField(required=True, max_length=20, help_text='')
+    street_address = db.StringField(required=True, help_text='location_on')
+    city = db.StringField(required=True, max_length=20, help_text='location_city')
+    state = db.StringField(required=True, max_length=20, help_text='navigation')
+    pincode = db.StringField(required=True, max_length=20, help_text='local_parking')
     school = db.StringField(required=True, max_length=100, help_text='')
 
     def __str__(self):
@@ -146,26 +146,26 @@ class Teacher(db.Document):
 
 class Profile(db.Document):
     user = db.StringField(required=True, max_length=50, help_text='')
-    phone = db.StringField(required=True, max_length=50, help_text='')
-    address = db.StringField(required=True, max_length=50, help_text='')
-    email = db.StringField(required=True, max_length=50, help_text='')
+    phone = db.StringField(required=True, max_length=50, help_text='phone')
+    address = db.StringField(required=True, max_length=50, help_text='location_on')
+    email = db.StringField(required=True, max_length=50, help_text='email')
     photo = db.StringField(required=True, max_length=50, help_text='')
 
 
 class Event(db.Document):
     school = db.StringField(required=True, max_length=50, help_text='')
-    event_name = db.StringField(required=True, max_length=50, help_text='')
-    from_date = db.StringField(required=True, max_length=50, help_text='')
-    to_date = db.StringField(required=True, max_length=50, help_text='')
-    start_time = db.StringField(required=True, max_length=50, help_text='')
-    end_time = db.StringField(required=True, max_length=50, help_text='')
-    location = db.StringField(required=True, max_length=50, help_text='')
+    event_name = db.StringField(required=True, max_length=50, help_text='perm_identity')
+    from_date = db.StringField(required=True, max_length=50, help_text='date_range')
+    to_date = db.StringField(required=True, max_length=50, help_text='date_range')
+    start_time = db.StringField(required=True, max_length=50, help_text='hourglass_full')
+    end_time = db.StringField(required=True, max_length=50, help_text='hourglass_empty')
+    location = db.StringField(required=True, max_length=50, help_text='location_on')
     event_for = db.StringField(required=True, verbose_name='Event is for',
                                choices=(('1', "Everyone"), ('2', "Students"), ('3', "Faculty"), ('4', "Parents")))
-    description = db.StringField(required=True)
+    description = db.StringField(required=True, help_text='description')
 
 
 class BulkNotification(db.Document):
     school = db.StringField(required=True, max_length=50, help_text='')
-    subject = db.StringField(required=True, max_length=200, help_text='')
-    body = db.StringField(required=True, verbose_name='Notification Message')
+    subject = db.StringField(required=True, max_length=200, help_text='mail_outline')
+    body = db.StringField(required=True, verbose_name='Notification Message', help_text='subject')
